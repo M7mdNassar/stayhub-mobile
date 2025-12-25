9b9ec289aa186ccd4ddff374bf89719fc59d00b2
Mohammed B Nassar <m.nassar4004@gmail.com>
2025-12-25 19:02:53 +0200

    Merge remote initial files

---
+++ StayHub/StayHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  StayHubApp.swift
+//  StayHub
+//
+//  Created by Mac on 25/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct StayHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
